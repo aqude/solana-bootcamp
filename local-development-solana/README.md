1a036a3b9f588c53cd95e2474d899545a9ff0193
Sergey <aquadefin@gmail.com>
2024-11-24 17:08:21 +0300

    stable versions

---
+++ local-development-solana/README.md
@@ -0,0 +1,3 @@
+# CLI autostarter for Solana development. 
+Windows + WSL && Linux development ready. 
+P.S before development check path to `solana-test-validator` or use `--solana_validator`
--- local-development-solana/src/main.rs
+++ local-development-solana/src/main.rs
@@ -1,7 +1,9 @@
 use clap::Parser;
+use std::io::Read;
+use std::thread::{self, spawn};
 use std::process::{Command, Stdio};
-const _PATH_TO_CONFIG: &str = "/etc/local-development-solana/config.toml";
-const _SITES: [&str; 2] = ["http://localhost:3000", "https://explorer.solana.com"];
+use std::time;
+use std::env;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -9,44 +11,90 @@ struct Args {
     /// full path to frontend folder
     #[arg(long)]
     frontend_folder: String,
+    /// full path to solana-test-validator executable file or use default path
+    #[arg(long, default_value_t = default_validator_path())]
+    solana_validator: String,
 }
 
+fn default_validator_path() -> String {
+    if cfg!(target_os = "windows") {
+        let formatted_command = format!("wsl -d Debian -e sh -c 'USER=$USER echo $USER'");
+        let mut cmd = Command::new("powershell")
+        .args(["/C", &formatted_command])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::inherit())
+        .spawn().expect("command \"GET USERNAME\" wasn't spawn");
+
+        let mut user = String::new();
+        if let Some(mut stdout) = cmd.stdout.take() {
+            let _ = stdout.read_to_string(&mut user).expect("Failed to read stdout");
+        }
+        user = user.trim().replace(" ", "");
+        format!("/home/{}/.local/share/solana/install/active_release/bin/solana-test-validator", user)
+    } else {
+        let user = env::var("USER").expect("USER environment variable not found");
+        format!("/home/{}/.local/share/solana/install/active_release/bin/solana-test-validator", user)
+    }
+
+}
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    const _PATH_TO_CONFIG: &str = "/etc/local-development-solana/config.toml";
+    const _SITES: [&str; 2] = ["http://localhost:3000", "https://explorer.solana.com"];
     let args: Args = Args::parse();
-    let command_frontend: String = format!("wsl -e sh -c cd {} && npm i && npm run dev", &args.frontend_folder);
-     //         --frontend-folder /home/aqude/projects/bootcamp-solana/voting-validation-app
-    // start frontend
-    command(&command_frontend);
-    // start validator
-    command("solana-test-validator");
-    // pen urls
-    for url in _SITES {
+    let command_frontend: String = format!("cd '{}' && npm i && npm run dev", &args.frontend_folder);
+    let command_solana = format!("cd ~/ && {}", &args.solana_validator);
+     let handle_one = spawn(move || {
+        let _ = command(&command_frontend);
+    });
+    let handle_two = spawn(move || {
+        let _ = command(&command_solana);
+    });
+    let handle_three = spawn(|| {
+        let _ = link_opener(&_SITES);
+    });
+  
+    handle_one.join().expect("Task one panicked");
+    handle_two.join().expect("Task two panicked");
+    handle_three.join().expect("Task three panicked");
+    command("/home/aqude/.local/share/solana/install/active_release/bin/solana-test-validator")?;
+    
+    Ok(())
+
+}
+
+fn link_opener(links: &[&str]) -> Result<(), Box<dyn std::error::Error>> {
+    let millis = time::Duration::from_millis(10000);
+    thread::sleep(millis);
+    for url in links {
         open::that(url)?
     }
+
     Ok(())
 }
 
-fn command(command: &str) {
+fn command(command: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd: Command;
-        if cfg!(target_os = "windows") {
-            cmd = Command::new("cmd");
-            cmd.arg("/C").arg(command)
-        } else {
-            cmd = Command::new("gnome-terminal");
-            cmd.args(["--", "sh", "-c", command])
-        };
+
+    if cfg!(target_os = "windows") {
+        let formatted_command = format!("wsl -d Debian -e sh -c \"{}\"", command);
+        cmd = Command::new("powershell");
+        cmd.args(["/C", &formatted_command])
+    } else {
+        cmd = Command::new("sh");
+        cmd.args([&command])
+    };
+
     let output = cmd
             .stdout(Stdio::inherit())
             .stderr(Stdio::inherit())
-            .spawn()
-            .expect("Failed to execute command")
-            .wait()
-            .expect("Command wasn't running");
+            .spawn()?
+            .wait()?;
     
-        if output.success() {
-            println!("Command executed successfully!");
-        } else {
-            eprintln!("Command failed with status: {:?}", output.code());
-        }
-    
+    if output.success() {
+        println!("Command executed successfully!");
+    } else {
+        eprintln!("Command failed with status: {:?}", output.code());
+    }
+
+    Ok(())
 }
